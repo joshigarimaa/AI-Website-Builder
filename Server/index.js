513bd4b943ccf4e507b6e680399e8d263a52062a
Shubhi <joshigarima906@gmail.com>
2026-02-28 22:45:33 +0530

    Initial project setup with server and database connection

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hey Shubhi");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
