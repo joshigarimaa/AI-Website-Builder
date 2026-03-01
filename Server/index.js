8e69c216ea0123de15ffa3e87a61178fd5d76d20
Shubhi <joshigarima906@gmail.com>
2026-03-01 13:27:21 +0530

    Added auth controller with login and register functionality

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,26 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+require("dotenv").config();
 
-dotenv.config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser()); // REQUIRED for cookies
+
+// connect database
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+// test route
 app.get("/", (req, res) => {
-  res.send("Hey Shubhi");
+  res.send("Server is running");
 });
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
